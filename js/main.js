0cdeb969e68c466ff74c7e6e31935b81f4d0a0ef
MTMiller <mtmiller@umd.edu>
2021-01-06 11:15:51 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,9 +3,9 @@ layout: null
 sitemap:
   exclude: 'yes'
 ---
-
 $(document).ready(function () {
   $('a.blog-button').click(function (e) {
+      $('.content-wrapper__inner').css('display', 'block')
     if ($('.panel-cover').hasClass('panel-cover--collapsed')) return
     currentWidth = $('.panel-cover').width()
     if (currentWidth < 960) {
@@ -19,10 +19,12 @@ $(document).ready(function () {
 
   if (window.location.hash && window.location.hash == '#blog') {
     $('.panel-cover').addClass('panel-cover--collapsed')
+      $('.content-wrapper__inner').css('display', 'block')
   }
 
   if (window.location.pathname !== '{{ site.baseurl }}/' && window.location.pathname !== '{{ site.baseurl }}/index.html') {
     $('.panel-cover').addClass('panel-cover--collapsed')
+      $('.content-wrapper__inner').css('display', 'block')
   }
 
   $('.btn-mobile-menu').click(function () {
